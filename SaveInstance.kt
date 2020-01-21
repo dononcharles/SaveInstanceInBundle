66e3cde1cba47de3a99deb917bf89bf054ae8083
Komi V. Donon <dononcharles@gmail.com>
2020-01-21 09:18:33 +0100

    Create SaveInstance.kt

---
+++ SaveInstance.kt
@@ -0,0 +1,9 @@
+com.app.package
+
+/**
+ * Created by Komi Donon on 1/21/2020.
+ */
+@MustBeDocumented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FIELD)
+annotation class SaveInstance
